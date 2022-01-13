aedd33df6c69f0e304ea9ac363dd71155c970277
soyeonnn <no02031@naver.com>
2022-01-13 11:40:38 +0900

    variable

---
+++ bin/Variables.dart
@@ -0,0 +1,26 @@
+void main() {
+  var name = 'myname';
+
+  /// name = 1;
+
+  dynamic message = 'Hello World';
+  message = 5;
+
+  const myName = "Oeckikek";
+
+  /// myName = "oeck";
+
+  final city = "Seoul";
+
+  /// city = "S";
+}
+
+// Inline comments(특정 줄의 내용에 대한 간략한 통찰력에 사용)
+
+/*
+Blocks of comments
+*/
+
+/// Documentation(코드를 문서화하는데 사용)
+/// 
+/// 
